1af86e1354b5d8a0eec4ff83fa527ba35acf0303
Julius Garrido <jflgarrido@gmail.com>
2021-12-08 20:46:03 -0700

    commit tree 21d4931cef3937a317f4c7266c530adcaccbf029

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i=0; i < array.length; i++){
+    const balance = target - array[i];
+    for (let j=i +1; j <array.length; j++)
+      if(array[j] === balance){
+        return true
+      }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  function hasTargetSum(array, target) {
+  // n steps
+  for (let i=0; i < array.length; i++){
+    const balance = target - array[i];
+    // n steps
+    for (let j=i +1; j <array.length; j++)
+      if(array[j] === balance){
+        return true
+      }
+  }
+  return false;
+  Big O notation => O(n*n) or O(n^2) or quadratic time complexity
 */
 
 /* 
-  Add your pseudocode here
+  Write a function that has arguments array and target
+    iterate through the array of numbers
+      For each number, know the balance between the target and the current number
+      Iterate through the rest of the numbers
+        See if there are numbers there's a number that is the same with the balance
+          Return true if there are matches
+  Otherwise, return false.
+
 */
 
 /*
@@ -24,6 +50,11 @@ if (require.main === module) {
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([13, 8, 4, 20], 17));
 
   console.log("");
 
